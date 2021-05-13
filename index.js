cd88a2eed8b72495530bf8a0041ea60833d9b354
darkcloud784 <mrkrodriguez@gmail.com>
2021-05-13 09:58:31 -0500

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fetch = require('node-fetch');
 
 const embed = new Discord.MessageEmbed()
   .setTitle('ayy')
-  .attachFiles(/app/TheKingBot/images/ayy.jpg)
+  .attachFiles("/app/TheKingBot/images/ayy.jpg")
   .setImage('attachment://ayy.jpg');
 
 const configPath = 'config.json';
@@ -111,7 +111,7 @@ client.on('message', msg => {
      msg.reply('https://bit.ly/3cLZouC');
    }
     if (msg.content.indexOf('ayy') >= 0 || msg.content.indexOf('Ayy') >= 0) {
-        var lmaos = ['ayy lmao! :alien:', 'remember the ayylmao! :alien: :face_with_cowboy_hat:', ('Your new name is ' + "@" + msg.author.username + 'ayy Lmao! :alien:'),   msg.channel.send({embed})];
+        var lmaos = ['ayy lmao! :alien:', 'remember the ayylmao! :alien: :face_with_cowboy_hat:', ('Your new name is ' + "@" + msg.author.username + 'ayy Lmao! :alien:'), msg.channel.send({embed})];
     var lmao = lmaos[Math.floor(Math.random() * lmaos.length)];
     msg.reply(lmao);
   }
